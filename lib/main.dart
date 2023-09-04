516890b3ce5d5b1bb7aaf366c1a726fe98a48d11
Sho Nagato <nagato.sho.178@gmail.com>
2023-09-04 11:43:25 +0900

    fix scafflod and appbar

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Flutter Demo',
-      home: Text(
-        'Hello Flutter!!',
-        style: TextStyle(fontSize: 25.0),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Hello Flutter!!'),
+        ),
+        body: const Text(
+          'Hello Flutter World!!',
+          style: TextStyle(fontSize: 25.0),
+        ),
       ),
     );
   }
